61b4cf8d20969cb552ce1c2dca52858249eb8697
MaybeSako <namedjobs@gmail.com>
2021-02-02 10:47:46 +0900

    Add a new function

--- apex_rank_tracker.py
+++ apex_rank_tracker.py
@@ -51,11 +51,17 @@ def callback():
 # 以下でWebhookから送られてきたイベントをどのように処理するかを記述する
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=get_stats())
+    )
+
+def get_stats():
     base_url = "https://public-api.tracker.gg/v2/apex/standard/"
     user_information = event.message.text
     endpoint = "profile/" + user_information
     session = requests.Session()
-    req = session.get(base_url+endpoint,params=params)
+    req = session.get(base_url+endpoint, params=params)
     req.close()
     res = json.loads(req.text)
 
@@ -63,16 +69,16 @@ def handle_message(event):
     rank_result = []
     player_id = res["data"]["platformInfo"]['platformUserId']
     player_level = res["data"]["segments"][0]["stats"]["level"]["displayValue"]
-    player_rank_name = res["data"]["segments"][0]["stats"]["rankScore"]["metadata"]["rankName"]
-    player_rp = res["data"]["segments"][0]["stats"]["rankScore"]["value"]
-    player_rank_position = res["data"]["segments"][0]["stats"]["rankScore"]["rank"]
+    rank_name = res["data"]["segments"][0]["stats"]["rankScore"]["metadata"]["rankName"]
+    ranked_point = res["data"]["segments"][0]["stats"]["rankScore"]["value"]
+    rank_position = res["data"]["segments"][0]["stats"]["rankScore"]["rank"]
     player_percentile = res["data"]["segments"][0]["stats"]["rankScore"]["percentile"]
 
     rank_result.append("ID: " + str(player_id))
     rank_result.append("Level: " + str(player_level))
-    rank_result.append("Rank: " + str(player_rank_name))
-    rank_result.append("RP: " + str(player_rp))
-    rank_result.append("Rank Position: " + str(player_rank_position) + "位")
+    rank_result.append("Rank: " + str(rank_name))
+    rank_result.append("RP: " + str(ranked_point))
+    rank_result.append("Rank Position: " + str(rank_position) + "位")
     # The value of percentile is occasioanlly returned as None
     if player_percentile is None:
         pass
@@ -80,10 +86,8 @@ def handle_message(event):
         rank_result.append("RP: " + str(player_percentile))
     rank_result = "\n".join(rank_result)
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=rank_result)
-    )
+    return rank_result
+
 
 # ポート番号の設定
 if __name__ == "__main__":
